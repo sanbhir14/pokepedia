ff200f29ce689f11b5661e43d76482ddbd940dec
sandi.bhirama <sbhirama@gmail.com>
2021-09-03 15:10:12 +0700

    redesign font

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 
-export const AppWrap = styled.div`  
+export const AppWrap = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap');
+    font-family: 'Abril Fatface', cursive;    
     width: 100%;
     justify-content:center;
     align-items:center;
