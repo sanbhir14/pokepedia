e90ba2d3bc4cc19321f8385dec392915fd7e0d5f
sandi.bhirama <sbhirama@gmail.com>
2021-09-03 15:03:38 +0700

    rebuild

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -1,12 +1,5 @@
 import styled from "styled-components";
-import { createGlobalStyle } from 'styled-components'
 
-const GlobalStyle = createGlobalStyle`
-
-   body {
-    
-  }
-`
 
 export const AppWrap = styled.div`
     @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap');
